e41c366c6a985c807b0995afecf03e2e08996be4
ajin15971@gmail.com <ajin15971@gmail.com>
2022-12-02 19:36:35 +0900

    fixed 1

--- my-app/pages/api/sensor/index.ts
+++ my-app/pages/api/sensor/index.ts
@@ -5,353 +5,48 @@ import { useEffect } from "react";
 const prisma = new PrismaClient();
 
 type Data = {
-  room413Co2?: any;
-  room413Humidity?: any;
-  room413Light?: any;
-  room413Pir?: any;
-  room413Temperature?: any;
-
-  room415Co2?: any;
-  room415Humidity?: any;
-  room415Light?: any;
-  room415Pir?: any;
-  room415Temperature?: any;
-
-  room419Co2?: any;
-  room419Humidity?: any;
-  room419Light?: any;
-  room419Pir?: any;
-  room419Temperature?: any;
-
-  room421Co2?: any;
-  room421Humidity?: any;
-  room421Light?: any;
-  room421Pir?: any;
-  room421Temperature?: any;
-
-  message?: any;
-  data413?: any;
-  data415?: any;
-  data417?: any;
-  data419?: any;
-  data421?: any;
+  co2Data?: any;
+  humidityData?: any;
+  lightData?: any;
+  pirData?: any;
+  temperatureData?: any;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  let room413Co2;
-  let room413Humidity;
-  let room413Light;
-  let room413Pir;
-  let room413Temperature;
-
-  let room415Co2;
-  let room415Humidity;
-  let room415Light;
-  let room415Pir;
-  let room415Temperature;
-
-  let room417Co2;
-  let room417Humidity;
-  let room417Light;
-  let room417Pir;
-  let room417Temperature;
-
-  let room419Co2;
-  let room419Humidity;
-  let room419Light;
-  let room419Pir;
-  let room419Temperature;
-
-  let room421Co2;
-  let room421Humidity;
-  let room421Light;
-  let room421Pir;
-  let room421Temperature;
-
-  room413Co2 = await prisma.co2.findMany({
-    where: {
-      roomNum: "413",
-    },
+  let co2Data = await prisma.co2.findMany({
     orderBy: {
       time: "asc",
     },
   });
 
-  // 415호 데이터
-  room413Humidity = await prisma.humidity.findMany({
-    where: {
-      roomNum: "413",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room413Light = await prisma.light.findMany({
-    where: {
-      roomNum: "413",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room413Pir = await prisma.pir.findMany({
-    where: {
-      roomNum: "413",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room413Temperature = await prisma.temperature.findMany({
-    where: {
-      roomNum: "413",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  // 415호 데이터
-  room415Co2 = await prisma.co2.findMany({
-    where: {
-      roomNum: "415",
-    },
+  let humidityData = await prisma.humidity.findMany({
     orderBy: {
       time: "asc",
     },
   });
 
-  room415Humidity = await prisma.humidity.findMany({
-    where: {
-      roomNum: "415",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room415Light = await prisma.light.findMany({
-    where: {
-      roomNum: "415",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room415Pir = await prisma.pir.findMany({
-    where: {
-      roomNum: "415",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room415Temperature = await prisma.temperature.findMany({
-    where: {
-      roomNum: "415",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  // 417호 데이터
-  room417Co2 = await prisma.co2.findMany({
-    where: {
-      roomNum: "417",
-    },
+  let lightData = await prisma.light.findMany({
     orderBy: {
       time: "asc",
     },
   });
 
-  room417Humidity = await prisma.humidity.findMany({
-    where: {
-      roomNum: "417",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room417Light = await prisma.light.findMany({
-    where: {
-      roomNum: "417",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room417Pir = await prisma.pir.findMany({
-    where: {
-      roomNum: "417",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room417Temperature = await prisma.temperature.findMany({
-    where: {
-      roomNum: "417",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  // 419데이터
-  room419Co2 = await prisma.co2.findMany({
-    where: {
-      roomNum: "419",
-    },
+  let pirData = await prisma.pir.findMany({
     orderBy: {
       time: "asc",
     },
   });
 
-  room419Humidity = await prisma.humidity.findMany({
-    where: {
-      roomNum: "419",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room419Light = await prisma.light.findMany({
-    where: {
-      roomNum: "419",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room419Pir = await prisma.pir.findMany({
-    where: {
-      roomNum: "419",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room419Temperature = await prisma.temperature.findMany({
-    where: {
-      roomNum: "419",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  // 421데이터
-  room421Co2 = await prisma.co2.findMany({
-    where: {
-      roomNum: "421",
-    },
+  let temperatureData = await prisma.temperature.findMany({
     orderBy: {
       time: "asc",
     },
   });
 
-  room421Humidity = await prisma.humidity.findMany({
-    where: {
-      roomNum: "421",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room421Light = await prisma.light.findMany({
-    where: {
-      roomNum: "421",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room421Pir = await prisma.pir.findMany({
-    where: {
-      roomNum: "421",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-  room421Temperature = await prisma.temperature.findMany({
-    where: {
-      roomNum: "421",
-    },
-    orderBy: {
-      time: "asc",
-    },
-  });
-
-  let data413 = [
-    room413Co2,
-    room413Humidity,
-    room413Light,
-    room413Pir,
-    room413Temperature,
-  ];
-
-  let data415 = [
-    room415Co2,
-    room415Humidity,
-    room415Light,
-    room415Pir,
-    room415Temperature,
-  ];
-
-  let data417 = [
-    room417Co2,
-    room417Humidity,
-    room417Light,
-    room417Pir,
-    room417Temperature,
-  ];
-
-  let data419 = [
-    room419Co2,
-    room419Humidity,
-    room419Light,
-    room419Pir,
-    room419Temperature,
-  ];
-
-  let data421 = [
-    room421Co2,
-    room421Humidity,
-    room421Light,
-    room421Pir,
-    room421Temperature,
-  ];
-
-  // console.log(room413Co2.length);
-  // console.log(data413[]);
-
-  // let data413 = [
-  //   room413Co2,
-  //   room413Humidity,
-  //   room413Light,
-  //   room413Pir,
-  //   room413Temperature,
-  //   room415Co2,
-  //   room415Humidity,
-  //   room415Light,
-  //   room415Pir,
-  //   room415Temperature,
-  //   room417Co2,
-  //   room417Humidity,
-  //   room417Light,
-  //   room417Pir,
-  //   room417Temperature,
-  //   room419Co2,
-  //   room419Humidity,
-  //   room419Light,
-  //   room419Pir,
-  //   room419Temperature,
-  //   room421Co2,
-  //   room421Humidity,
-  //   room421Light,
-  //   room421Pir,
-  //   room421Temperature,
-  // ];
-
-  res.status(200).json({ data413, data415, data417, data419, data421 });
+  res
+    .status(200)
+    .json({ co2Data, humidityData, lightData, pirData, temperatureData });
 }
--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -3,14 +3,79 @@ import Line from "../components/chart";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [room413, setRoom413] = useState<any>("");
-  const [room415, setRoom415] = useState<any>("");
-  const [room417, setRoom417] = useState<any>("");
-  const [room419, setRoom419] = useState<any>("");
-  const [room421, setroom421] = useState<any>("");
+  const [apiCo2Data, setApiCo2Data] = useState<any>([]);
+  const [apiHumidityData, setApiHumidityData] = useState<any>([]);
+  const [apiLightData, setApiLightData] = useState<any>([]);
+  const [apiPirData, setApiPirData] = useState<any>([]);
+  const [apiTemperatureData, setApiTemperatureData] = useState<any>([]);
 
-  const temp = [room413, room415, room417, room419, room421];
-  console.log(temp);
+  const a = () => {
+    fetch(`/api/sensor`)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        console.log(json.co2Data);
+
+        setApiCo2Data(json.co2Data);
+        setApiHumidityData(json.humidityData);
+        setApiLightData(json.lightData);
+        setApiPirData(json.pitData);
+        setApiTemperatureData(json.temperatureData);
+      });
+  };
+  useEffect(() => {
+    a();
+  }, []);
+
+  // console.log(apiCo2Data[0].roomNum == "415" ? "성공" : "실패");
+
+  // console.log(
+  //   apiCo2Data[0].roomNum == "417"
+  //     ? "성공"
+  //     : apiCo2Data[0].roomNum == "419"
+  //     ? "성공"
+  //     : apiCo2Data[0].roomNum == "421"
+  //     ? "성공"
+  //     : apiCo2Data[0].roomNum == "415"
+  //     ? "123313123213"
+  //     : "실패"
+  // );
+
+  // console.log(apiCo2Data[0].rommNum);
+  // console.log(417 ? apiCo2Data[0].roomNum : "null");
+
+  let co2Room413: any = [];
+  let co2Room415: any = [];
+  let co2Room417: any = [];
+  let co2Room419: any = [];
+  let co2Room421: any = [];
+  let aaa: any = [];
+
+  const roomsCo2 = apiCo2Data.map((co2: any, idx: any) => {
+    co2.roomNum == "413"
+      ? co2Room413.push(co2)
+      : co2.roomNum == "415"
+      ? co2Room415.push(co2)
+      : co2.roomNum == "417"
+      ? co2Room417.push(co2)
+      : co2.roomNum == "419"
+      ? co2Room419.push(co2)
+      : co2.roomNum == "421"
+      ? co2Room421.push(co2)
+      : null;
+
+    if (co2Room413.length > 5) {
+      co2Room413.splice(0, 1);
+    }
+    // ##############여기서부터 하면됩니다. 5개씩 넘기세요
+    // console.log(co2Room413.length);
+
+    return co2Room413;
+  });
+  console.log(roomsCo2);
+
+  // const temp = [room413, room415, room417, room419, room421];
+  // console.log(temp);
 
   // const [members, setMembers] = useState<Array<string>>([]);
 
@@ -21,111 +86,18 @@ export default function Home() {
   //   console.log(Object.is(tmp, co2)); // false
   //   setCo2(tmp); // rerendering o
   // };
-  const a = temp.map((temp: any, index: number) => `${temp} ${index}`);
+  // const a = temp.map((temp: any, index: number) => `${temp} ${index}`);
   // console.log(a);
 
-  useEffect(() => {
-    fetch(`/api/sensor`)
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json.data413);
+  // const apiData = () => {
 
-        // let co2abc = json.data413[0].map();
-
-        // setTimeout(() => {
-        // let co2A = json.data413[0].map((ele: any, idx: number) => {
-        // let co2A = ele.co2;
-
-        // return co2A;
-        // return co2A;
-        // });
-        // return co2A;
-        // }, 5000);
-        // console.log(co2A);
-
-        let co2A = json.data413[0][0].co2;
-        let humidityA = json.data413[1][0].humidity;
-        let lightA = json.data413[2][0].light;
-        let pirA = json.data413[3][0].pir;
-        let temperatureA = json.data413[4][0].temperature;
-
-        let co2B = json.data415[0][0].co2;
-        let humidityB = json.data415[1][0].humidity;
-        let lightB = json.data415[2][0].light;
-        let pirB = json.data415[3][0].pir;
-        let temperatureB = json.data415[4][0].temperature;
-
-        let co2C = json.data417[0][0].co2;
-        let humidityC = json.data417[1][0].humidity;
-        let lightC = json.data417[2][0].light;
-        let pirC = json.data417[3][0].pir;
-        let temperatureC = json.data417[4][0].temperature;
-
-        let co2D = json.data419[0][0].co2;
-        let humidityD = json.data419[1][0].humidity;
-        let lightD = json.data419[2][0].light;
-        let pirD = json.data419[3][0].pir;
-        let temperatureD = json.data419[4][0].temperature;
-
-        let co2E = json.data421[0][0].co2;
-        let humidityE = json.data421[1][0].humidity;
-        let lightE = json.data421[2][0].light;
-        let pirE = json.data421[3][0].pir;
-        let temperatureE = json.data421[4][0].temperature;
-
-        // let co2A = json.data413[0][0].co2;
-        // let co2B = json.data415[0][0].co2;
-        // let co2C = json.data417[0][0].co2;
-        // let co2D = json.data419[0][0].co2;
-        // let co2E = json.data421[0][0].co2;
-
-        // let humidityA = json.data413[1][0].humidity;
-        // let humidityB = json.data415[1][0].humidity;
-        // let humidityC = json.data417[1][0].humidity;
-        // let humidityD = json.data419[1][0].humidity;
-        // let humidityE = json.data421[1][0].humidity;
-
-        // let lightA = json.data413[2][0].light;
-        // let lightB = json.data415[2][0].light;
-        // let lightC = json.data417[2][0].light;
-        // let lightD = json.data419[2][0].light;
-        // let lightE = json.data421[2][0].light;
-
-        // let pirA = json.data413[3][0].pir;
-        // let pirB = json.data415[3][0].pir;
-        // let pirC = json.data417[3][0].pir;
-        // let pirD = json.data419[3][0].pir;
-        // let pirE = json.data421[3][0].pir;
-
-        // let temperatureA = json.data413[4][0].temperature;
-        // let temperatureB = json.data415[4][0].temperature;
-        // let temperatureC = json.data417[4][0].temperature;
-        // let temperatureD = json.data419[4][0].temperature;
-        // let temperatureE = json.data421[4][0].temperature;
-
-        let data413 = [co2A, humidityA, lightA, pirA, temperatureA];
-        let data415 = [co2B, humidityB, lightB, pirB, temperatureB];
-        let data417 = [co2C, humidityC, lightC, pirC, temperatureC];
-        let data419 = [co2D, humidityD, lightD, pirD, temperatureD];
-        let data421 = [co2E, humidityE, lightE, pirE, temperatureE];
-
-        data413.map((ele, idx) => {});
-        // console.log(String(Number(a[0].co2)));
-        // setCo2([co2Data.concat(co2Data)]);
-        console.log(data413);
-        setRoom413(data413);
-        setRoom415(data415);
-        setRoom417(data417);
-        setRoom419(data419);
-        setroom421(data421);
-      });
-  }, []);
+  // };
 
   return (
     <div className="bg-red-200 flex justify-center  ">
       <div className="bg-blue-200 w-[800px] h-[800px] flex items-center">
         {/* {co2.map((co2: string, index: number) => ( */}
-        <Line data={temp}></Line>
+        {/* <Line data={temp}></Line> */}
         {/* <Line data={temp}></Line> */}
         {/* ))} */}
       </div>
